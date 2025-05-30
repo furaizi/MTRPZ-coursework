7d8629fc1ecb690f7c44a25c5c0556a75293a2ef
furaizi <furachannel16@gmail.com>
2025-05-30 21:37:47 +0300

    Establish NetworkModule object

--- app/src/main/java/com/example/urlshortenerandroid/di/NetworkModule.kt
+++ app/src/main/java/com/example/urlshortenerandroid/di/NetworkModule.kt
@@ -1,2 +1,34 @@
 package com.example.urlshortenerandroid.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = ""
+
+    @Provides @Singleton
+    fun okHttp(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        }).build()
+
+    @Provides
+    @Singleton
+    fun retrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun linkApi(retrofit: Retrofit): LinkApi = retrofit.create(LinkApi::class.java)
+}
