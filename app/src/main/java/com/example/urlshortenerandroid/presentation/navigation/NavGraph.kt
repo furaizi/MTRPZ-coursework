926356973943a18a7895cb830219f7845fad23d9
furaizi <furachannel16@gmail.com>
2025-05-30 21:31:47 +0300

    Establish NavGraph composable function

--- app/src/main/java/com/example/urlshortenerandroid/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/urlshortenerandroid/presentation/navigation/NavGraph.kt
@@ -2,8 +2,30 @@ package com.example.urlshortenerandroid.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 @Composable
 fun NavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(navController, startDestination = Destinations.Shorten.name) {
+        composable(Destinations.Shorten.name)   { ShortenScreen { id ->
+            navController.navigate("${Destinations.Details.name}/$id") } }
+
+        composable(
+            route = "${Destinations.Details.name}/{id}",
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val linkId = backStackEntry.arguments!!.getString("id")!!
+            DetailsScreen(linkId,
+                onStatsClick = { navController.navigate("${Destinations.Stats.name}/$linkId") })
+        }
+
+        composable(
+            route = "${Destinations.Stats.name}/{id}",
+            arguments = listOf(navArgument("id") { type = NavType.StringType })
+        ) { StatsScreen(it.arguments!!.getString("id")!!) }
+    }
 }
