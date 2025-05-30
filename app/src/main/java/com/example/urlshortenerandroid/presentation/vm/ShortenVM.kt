d31bab4261989bf0ef1fdd93d0b9a468f7161468
furaizi <furachannel16@gmail.com>
2025-05-30 21:54:46 +0300

    Establish ShortenVM class

--- app/src/main/java/com/example/urlshortenerandroid/presentation/vm/ShortenVM.kt
+++ app/src/main/java/com/example/urlshortenerandroid/presentation/vm/ShortenVM.kt
@@ -1,2 +1,40 @@
-package com.example.urlshortenerandroid.presentation.vm
+package com.example.urlshortener.presentation.vm
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.urlshortener.domain.model.Link
+import com.example.urlshortener.domain.usecase.CreateLinkUC
+import com.example.urlshortener.util.UiState
+import com.example.urlshortenerandroid.data.remote.dto.LinkResponse
+import com.example.urlshortenerandroid.util.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * VM для экрана "Сократить ссылку".
+ *
+ * ― Хранит UiState<Link>.
+ * ― Вызывает UseCase для создания ссылки.
+ */
+@HiltViewModel
+class ShortenVM @Inject constructor(
+    private val createLink: CreateLinkUC
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<LinkResponse>>(UiState.Success(null))
+    val uiState = _uiState.asStateFlow()
+
+    fun shorten(url: String) = viewModelScope.launch {
+        _uiState.value = UiState.Loading
+        createLink(url).fold(
+            onSuccess = { link -> _uiState.value = UiState.Success(link) },
+            onFailure = { e   -> _uiState.value = UiState.Error(e.localizedMessage ?: "Неизвестная ошибка") }
+        )
+    }
+
+    fun reset() { _uiState.value = UiState.Success(null) }
+}
--- app/src/main/java/com/example/urlshortenerandroid/util/UiState.kt
+++ app/src/main/java/com/example/urlshortenerandroid/util/UiState.kt
@@ -2,6 +2,6 @@ package com.example.urlshortenerandroid.util
 
 sealed interface UiState<out T> {
     data object Loading : UiState<Nothing>
-    data class Success<out T>(val data: T) : UiState<T>
+    data class Success<out T>(val data: T?) : UiState<T>
     data class Error(val msg: String, val code: Int? = null): UiState<Nothing>
 }
