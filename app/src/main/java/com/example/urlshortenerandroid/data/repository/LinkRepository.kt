a700a7d027923a00bd186b331520ae016fd0e2d2
furaizi <furachannel16@gmail.com>
2025-05-30 21:49:01 +0300

    Establish LinkRepository class

--- app/src/main/java/com/example/urlshortenerandroid/data/repository/LinkRepository.kt
+++ app/src/main/java/com/example/urlshortenerandroid/data/repository/LinkRepository.kt
@@ -1,2 +1,26 @@
 package com.example.urlshortenerandroid.data.repository
 
+import com.example.urlshortenerandroid.data.remote.LinkApi
+import com.example.urlshortenerandroid.data.remote.dto.CreateLinkRequest
+import com.example.urlshortenerandroid.data.remote.dto.LinkResponse
+import com.example.urlshortenerandroid.data.remote.dto.LinkStatistics
+import jakarta.inject.Inject
+
+class LinkRepository @Inject constructor(private val api: LinkApi) {
+
+    suspend fun create(url: String): Result<LinkResponse> = runCatching {
+        api.create(CreateLinkRequest(url))
+    }
+
+    suspend fun details(id: String): Result<LinkResponse> = runCatching {
+        api.details(id)
+    }
+
+    suspend fun stats(id: String): Result<LinkStatistics> = runCatching {
+        api.stats(id)
+    }
+
+    suspend fun delete(id: String) = runCatching {
+        api.delete(id)
+    }
+}
