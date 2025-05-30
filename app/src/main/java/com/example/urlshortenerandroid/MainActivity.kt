e6b955cfd59066080477f6f6e384e935daaa5e05
furaizi <furachannel16@gmail.com>
2025-05-30 23:33:22 +0300

    Remove unused imports in MainActivity class

--- app/src/main/java/com/example/urlshortenerandroid/MainActivity.kt
+++ app/src/main/java/com/example/urlshortenerandroid/MainActivity.kt
@@ -3,16 +3,8 @@ package com.example.urlshortenerandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.urlshortenerandroid.ui.theme.URLShortenerAndroidTheme
+import com.example.urlshortenerandroid.presentation.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
