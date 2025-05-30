ee8d9e43cfb14a8e07bea02b6491dfb574d3797c
furaizi <furachannel16@gmail.com>
2025-05-30 21:40:00 +0300

    Establish LinkApi interface

--- app/src/main/java/com/example/urlshortenerandroid/data/remote/LinkApi.kt
+++ app/src/main/java/com/example/urlshortenerandroid/data/remote/LinkApi.kt
@@ -1,2 +1,22 @@
 package com.example.urlshortenerandroid.data.remote
 
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface LinkApi {
+    @POST("links")
+    suspend fun create(@Body request: CreateLinkRequest): LinkResponse
+
+    @GET("links/{shortCode}")
+    suspend fun details(@Path("shortCode") shortCode: String): LinkResponse
+
+    @GET("links/{shortCode}/stats")
+    suspend fun stats(@Path("shortCode") shortCode: String): LinkStatistics
+
+    @DELETE("links/{shortCode}")
+    suspend fun delete(@Path("shortCode") shortCode: String): Response<Unit>
+}
