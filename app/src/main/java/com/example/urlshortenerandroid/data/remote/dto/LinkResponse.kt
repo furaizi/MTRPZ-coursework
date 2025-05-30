2e45805b3625c71af1c58ec513e88222acba29aa
furaizi <furachannel16@gmail.com>
2025-05-30 21:41:33 +0300

    Establish LinkResponse DTO class

--- app/src/main/java/com/example/urlshortenerandroid/data/remote/dto/LinkResponse.kt
+++ app/src/main/java/com/example/urlshortenerandroid/data/remote/dto/LinkResponse.kt
@@ -1,2 +1,13 @@
 package com.example.urlshortenerandroid.data.remote.dto
 
+import com.squareup.moshi.JsonClass
+import java.time.LocalDateTime
+
+@JsonClass(generateAdapter = true)
+data class LinkResponse(
+    val shortCode: String,
+    val url: String,
+    val originalUrl: String,
+    val expiresAt: LocalDateTime?,
+    val createdAt: LocalDateTime
+)
