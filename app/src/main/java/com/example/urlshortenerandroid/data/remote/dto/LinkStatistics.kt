42edbc3378fea3885b3df7dde4592def1f36c332
furaizi <furachannel16@gmail.com>
2025-05-30 21:42:23 +0300

    Establish LinkStatistics DTO class

--- app/src/main/java/com/example/urlshortenerandroid/data/remote/dto/LinkStatistics.kt
+++ app/src/main/java/com/example/urlshortenerandroid/data/remote/dto/LinkStatistics.kt
@@ -1,2 +1,13 @@
 package com.example.urlshortenerandroid.data.remote.dto
 
+import com.squareup.moshi.JsonClass
+import java.time.LocalDateTime
+
+@JsonClass(generateAdapter = true)
+data class LinkStatistics(
+    val shortCode: String,
+    val clicks: Long,
+    val uniqueVisitors: Long,
+    val lastAccessedAt: LocalDateTime?,
+    val isActive: Boolean
+)
